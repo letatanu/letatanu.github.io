974a2ec88a5969019d21b300f7c9168c65509557
letatanu <letatanu@gmail.com>
2026-01-01 15:16:11 -0500

    update google scholar

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -1,15 +1,25 @@
-from scholarly import scholarly
+from scholarly import scholarly, ProxyGenerator
 import jsonpickle
 import json
 from datetime import datetime
 import os
 
+# 1. Set up a Proxy Generator to avoid blocking
+pg = ProxyGenerator()
+success = pg.FreeProxies() # Uses free proxies
+scholarly.use_proxy(pg)
+
+# 2. Fetch Author Data
 author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
 scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
+
+# 3. Update Data
 name = author['name']
 author['updated'] = str(datetime.now())
 author['publications'] = {v['author_pub_id']:v for v in author['publications']}
 print(json.dumps(author, indent=2))
+
+# 4. Save Results
 os.makedirs('results', exist_ok=True)
 with open(f'results/gs_data.json', 'w') as outfile:
     json.dump(author, outfile, ensure_ascii=False)
